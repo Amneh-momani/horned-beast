5f2649e58fb22019e4cddf7e3bec6a9b9a506918
Amneh-momani <Amneh99el@gmail.com>
2021-07-28 23:24:20 -0700

    class3 part2

--- src/App.js
+++ src/App.js
@@ -1,28 +1,57 @@
-import "./App.css";
+import React from "react";
 import Main from "./components/Main";
+import Horned from "./components/horned.json";
 import Header from "./components/Header";
 import Footer from "./components/Footer";
+import SelectedBeast from "./components/SelectedBeast ";
 
-function App() {
+class App extends React.Component {
   constructor(props) {
     super(props);
     this.state = {
-      click: 20,
+      visible: false,
+      title: null,
+      image_url: null,
+      description: null,
+      count: 0,
     };
   }
 
-  getclickedImg = () => {
+  modalData = () => {
     this.setState({
-      click: this.state.click - 1,
+      title: null,
+      image_url: null,
+      description: null,
+      count: 0,
     });
   };
-  return (
-    <div>
-      <Header />
-      <Main /> {this.state.cookiesJar}
-      <Footer />
-    </div>
-  );
-}
 
+  modalUpdateVisible = () => {
+    this.setState({
+      visible: this.state.visible
+    });
+  };
+
+  render() {
+    return (
+      <div>
+        <SelectedBeast
+          title={this.state.title}
+          image_url={this.state.image_url}
+          description={this.state.description}
+          visible={this.state.visible}
+          modalUpdateVisible={this.modalUpdateVisible}
+          modalData={this.modalData}
+        />
+        <Header />
+        <Main
+          data={Horned}
+          modalUpdateVisible={this.modalUpdateVisible}
+          modalData={this.modalData}
+        />
+        <Footer />
+      </div>
+    );
+  }
+}
 export default App;
